0b334bcde337ab725a755f333665009dd2407dd5
Mozfet <wynand@expertbox.com>
2018-12-13 16:55:18 +0200

    upgrade to materializecss v1

--- package.js
+++ package.js
@@ -10,8 +10,8 @@ Npm.depends({
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@1.8');
-  api.use(['ecmascript','templating', 'underscore', 'reactive-var', 'blaze'],
-    'client');
+  api.use(['ecmascript','templating@1.3.2', 'underscore', 'reactive-var',
+    'blaze@2.3.3'], 'client');
   api.use('fourseven:scss@4.10.0');
   api.use('aldeed:autoform@6.3.0');
   api.addFiles([
